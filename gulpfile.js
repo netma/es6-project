18b27797c77591eb3e1e3e8fe794aeb1afa1bee7
Ateliers Nomades <info@iMac-de-Ateliers.local>
2017-12-04 15:36:33 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var babelify = require('babelify');
+var transform = require('vinyl-transform');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
